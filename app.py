e12422f5895090287a556366239f21f53b234ac4
devmuqaddas <talhaaliwise@gmail.com>
2025-11-12 11:37:38 +0500

    update comments

--- app.py
+++ app.py
@@ -376,7 +376,6 @@ async def health_check():
     }
 
 if __name__ == "__main__":
-    import os
     import uvicorn
-    port = int(os.environ.get("PORT", 5000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=Tru
+    port = int(os.environ.get("PORT", 5000))  # Railway dynamic port
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
